44ea713c8fa25e2c8cb0177560cd063e9064308a
Artem Besedin <speker@mail.ru>
2019-07-25 21:36:22 +0400

    Add several methods

--- src/App.php
+++ src/App.php
@@ -2,4 +2,22 @@
     public function __construct() {
         return '';
     }
+
+    public function run() {
+        return '';
+    }
+
+    public function foo() {
+        return 'abc';
+    }
+
+    public function bar() {
+        return 'sss';
+    }
+
+    public function getTotal() {
+        if(false){
+            return 'sss';
+        }
+    }
 }
